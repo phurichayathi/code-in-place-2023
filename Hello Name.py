bcc0fe52b8c1ef0f4a4ef7f99e865bab9bb810d6
phurichayathi <126574586+phurichayathi@users.noreply.github.com>
2023-08-03 00:43:07 +0700

    Create Hello Name.py

---
+++ Hello Name.py
@@ -0,0 +1,9 @@
+# Write a customizable version of the classic "hello world!" program in main.py
+
+def main():
+    # This print to the console
+    user_answer = input("What is your name? ")
+    print("Hello " + user_answer)
+
+if __name__ == '__main__':
+    main()
